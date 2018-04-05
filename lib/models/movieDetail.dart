ba0b1eda6a8395f9bf785d832aa0f6b7c13ad198
Rakesh Das <rakeshdaskowshik@gmail.com>
2018-04-04 20:57:03 -0500

    Added model for movie details

---
+++ lib/models/movieDetail.dart
@@ -0,0 +1,31 @@
+class MovieDetail {
+  final String title;
+  final double rating;
+  final String posterArtUrl;
+  final backgroundArtUrl;
+  final String overview;
+  final String tagline;
+  final int id;
+
+  const MovieDetail(
+      {this.title, this.rating, this.posterArtUrl, this.backgroundArtUrl, this.overview, this.tagline, this.id});
+
+  MovieDetail.fromJson(Map jsonMap)
+      : title = jsonMap['title'],
+        rating = jsonMap['vote_average'].toDouble(),
+        posterArtUrl = "http://image.tmdb.org/t/p/w342" + jsonMap['backdrop_path'],
+        backgroundArtUrl = "http://image.tmdb.org/t/p/w500" + jsonMap['poster_path'],
+        overview = jsonMap['overview'],
+        tagline = jsonMap['tagline'],
+        id = jsonMap['id'];
+}
+
+class FetchDataException implements Exception {
+  String _message;
+
+  FetchDataException(this._message);
+
+  String toString() {
+    return "Exception: $_message";
+  }
+}
