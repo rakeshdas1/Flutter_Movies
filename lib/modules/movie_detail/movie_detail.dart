7695e5b85602e3c5ab6f8f95eef248fd70e82bc1
Rakesh Das <rakeshdaskowshik@gmail.com>
2018-05-14 12:55:27 -0500

    added sliver appbar

--- lib/modules/movie_detail/movie_detail.dart
+++ lib/modules/movie_detail/movie_detail.dart
@@ -33,28 +33,59 @@ class _MovieDetailState extends State<MovieDetail> {
   Movie movie;
 
   _MovieDetailState(this.movie);
+  final double _appBarHeight = 256.0;
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new SingleChildScrollView(
-        child: new Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            MovieHeader(movie),
-            Padding(
-              padding: const EdgeInsets.only(left: 25.0),
-              child: Text(movie.title,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 28.0,
-                  )),
+      // body: new SingleChildScrollView(
+      //   child: new Column(
+      //     crossAxisAlignment: CrossAxisAlignment.start,
+      //     mainAxisSize: MainAxisSize.max,
+      //     mainAxisAlignment: MainAxisAlignment.end,
+      //     children: <Widget>[
+      //       MovieHeader(movie),
+      //       Padding(
+      //         padding: const EdgeInsets.only(left: 25.0),
+      //         child: Text(movie.title,
+      //             style: TextStyle(
+      //               fontWeight: FontWeight.bold,
+      //               fontSize: 28.0,
+      //             )),
+      //       ),
+      //       RatingInfo(movie),
+      //     ],
+      //   ),
+      // ),
+
+      body: new CustomScrollView(
+        slivers: <Widget>[
+          new SliverAppBar(
+            expandedHeight: _appBarHeight,
+            pinned: true,
+            flexibleSpace: new FlexibleSpaceBar(
+              title: Text(movie.title),
+              background: new Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  new Hero(
+                      tag: movie.id,
+                      child: new Image.network(
+                        movie.posterArtUrl,
+                        fit: BoxFit.cover,
+                        height: _appBarHeight,
+                      ))
+                ],
+              ),
             ),
-            RatingInfo(movie),
-          ],
-        ),
+          ),
+          new SliverList(
+            delegate: new SliverChildListDelegate(<Widget>[
+              
+
+            ]),
+          )
+        ],
       ),
     );
   }
